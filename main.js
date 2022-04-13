9b4b2ae15abff4385940c118aea8977a7a4a07df
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-13 14:25:48 -0400

    Grabbing all of the different voices and mapping over the voices and then grabbing it voice and adding it as an option for the voice dropdown

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ const voicesDropdown = document.querySelector('[name="voice"]')
 const options = document.querySelectorAll('[type="range"], [name="text"]')
 const speakButton = document.querySelector('#speak')
 const stopButton = document.querySelector('#stop')
+
+
+msg.text = document.querySelector('[name=text]').value
+
+function populateVoices() {
+  voices = this.getVoices()
+  const voiceOptions = voices.map((voice) => `<option value="${voice.name}">${voice.name}</option>`
+  ).join("")
+  voicesDropdown.innerHTML = voiceOptions
+}
+
+speechSynthesis.addEventListener('voiceschanged', populateVoices)
